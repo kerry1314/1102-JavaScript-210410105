b55c118f7ee75896f9197ead61d49eeed370fd07
kerry172128 <210410105@gms.tku.edu.tw>
2022-05-18 20:55:44 +0800

    w13-p4: use async/await to get people.json, and show it on webpage

---
+++ demo/W13/5_json-people-async-await/app.js
@@ -0,0 +1,73 @@
+const btn = document.querySelector('.btn');
+const url ='./api/people.json'
+
+btn.addEventListener('click', ()=> {
+    fetchDataAsync(url);
+});
+
+const fetchDataAsync = async (url) =>{
+    try{
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log('data',data);
+        displayItems(data);
+    }catch(err){
+        console.log('err',err);
+    }
+    
+}
+
+
+function fetchData(url){
+    // const response = fetch(url).then().catch();
+    // console.log('response', response);
+
+    fetch(url).then((response)=> response.json())
+    .then((data)=> {console.log('data', data);
+displayItems(data);})
+    .catch((err)=> console.log('err',err));
+}
+
+const displayItems = (items) => {
+    const displayData = items.map((item)=>{
+        return `<p>${item.name}</p>`; 
+    }).join('');
+
+    console.log('displayData', displayData);
+    const element = document.createElement('div');
+    element.innerHTML = displayData;
+    document.body.appendChild(element);
+}
+
+function getData(url){
+    const xhr = new XMLHttpRequest();
+    console.log('xhr0', xhr);
+    
+    xhr.open('GET', url);
+    xhr.onreadystatechange = function() {
+        console.log('xhr', xhr);
+        if(xhr.readyState === 4 && xhr.status === 200){
+            // console.log('done');
+            const data = JSON.parse(xhr.responseText);
+            console.log('data',data);
+            const displayData = data.map((item)=>{
+                return `<p>${item.name}</p>`; 
+            }).join('');
+
+            console.log('displayData', displayData);
+            const element = document.createElement('div');
+            element.innerHTML = displayData;
+            document.body.appendChild(element);
+        }else{
+            console.log({
+                status: xhr.status,
+                text: xhr.statusText,
+            });
+        }
+    };
+    
+    xhr.send();
+}
+
+
+console.log('hello');
