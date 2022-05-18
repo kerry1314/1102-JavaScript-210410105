21809498d15e14d9b5329a8a6a4b21b655ebd30d
kerry172128 <210410105@gms.tku.edu.tw>
2022-05-18 20:33:16 +0800

    w13-p3: use fetch api to get people.json, and show it on webpage

---
+++ demo/W13/4_json-people-fetch-api/app.js
@@ -0,0 +1,60 @@
+const btn = document.querySelector('.btn');
+const url ='./api/people.json'
+
+btn.addEventListener('click', ()=> {
+    fetchData(url);
+});
+
+function fetchData(url){
+    // const response = fetch(url).then().catch();
+    // console.log('response', response);
+
+    fetch(url).then((response)=> response.json())
+    .then((data)=> {console.log('data', data);
+displatItems(data);})
+    .catch((err)=> console.log('err',err));
+}
+
+const displatItems = (items) => {
+    const displayData = items.map((item)=>{
+        return `<p>${item.name}</p>`; 
+    }).join('');
+
+    console.log('displayData', displayData);
+    const element = document.createElement('div');
+    element.innerHTML = displayData;
+    document.body.appendChild(element);
+}
+
+function getData(url){
+    const xhr = new XMLHttpRequest();
+    console.log('xhr0', xhr);
+    
+    xhr.open('GET', url);
+    xhr.onreadystatechange = function() {
+        console.log('xhr', xhr);
+        if(xhr.readyState === 4 && xhr.status === 200){
+            // console.log('done');
+            const data = JSON.parse(xhr.responseText);
+            console.log('data',data);
+            const displayData = data.map((item)=>{
+                return `<p>${item.name}</p>`; 
+            }).join('');
+
+            console.log('displayData', displayData);
+            const element = document.createElement('div');
+            element.innerHTML = displayData;
+            document.body.appendChild(element);
+        }else{
+            console.log({
+                status: xhr.status,
+                text: xhr.statusText,
+            });
+        }
+    };
+    
+    xhr.send();
+}
+
+
+console.log('hello');
