f542ecad53ec70ed5bce4831ac375d5e3e4636fa
kerry172128 <210410105@gms.tku.edu.tw>
2022-06-08 19:16:30 +0800

    w16-p1: finish displaySingleDrink_xx.js

---
+++ demo/W16/cocktaildb_05/singleDrink_05.js
@@ -0,0 +1,19 @@
+import fetchDrinks from "./src/fetchDrinks_05.js"
+
+import displaysingleDrink from './src/displaySingleDrink_05.js'
+
+const singleDrink = async () => {
+    const id = localStorage.getItem('drink');
+    console.log('local id',id);
+    if(!id){
+        window.location.replace('index.html');
+    }else{
+        const drink = await fetchDrinks(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`);
+        console.log('single drink',drink);
+        displaysingleDrink(drink);
+    };
+};
+
+window.addEventListener('DOMContentLoaded', ()=>{
+    singleDrink();
+});
