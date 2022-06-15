9affdfebcf4ad198fd72e8f21bfd2aec3ab3f747
kerry172128 <210410105@gms.tku.edu.tw>
2022-06-15 20:40:57 +0800

    w17-p3: search items that is available in searchItems array

--- demo/W17/cocktaildb_05/app_05.js
+++ demo/W17/cocktaildb_05/app_05.js
@@ -4,7 +4,7 @@ import './src/searchForm_05.js';
 
 import { LOCAL_URL, hasNetwork,searchItems } from './api/search/config_05.js'
 
-console.log('config_05.js', { LOCAL_URL, hasNetwork,searchItems });
+// console.log('config_05.js', { LOCAL_URL, hasNetwork,searchItems });
 
 const URL = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=ke`
 
@@ -12,6 +12,6 @@ window.addEventListener('DOMContentLoaded', ()=> {
   if (hasNetwork){
     presentDrinks(URL);
   }else{
-    presentDrinks(LOCAL_URL);
+    presentDrinks(LOCAL_URL+'ke.json');
   }
 });
--- demo/W17/cocktaildb_05/src/searchForm_05.js
+++ demo/W17/cocktaildb_05/src/searchForm_05.js
@@ -1,12 +1,17 @@
 import get from './getElement.js';
 import presentDrinks from './presentDrinks_05.js';
 
+import { LOCAL_URL, hasNetwork, searchItems } from '../api/search/config_05.js';
+
 const form = get('.search-form');
 
 const input = get('[name="drink"]');
 
 // console.log('form',form);
 // console.log('input',input);
+
+console.log('config_05.js', { LOCAL_URL, hasNetwork,searchItems });
+
 
 const baseURL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 
@@ -15,5 +20,11 @@ form.addEventListener('keyup',(e)=>{
     console.log('input',input.value);
     const value = input.value;
     if(!value) return;
+    if(hasNetwork){
         presentDrinks(`${baseURL}${value}`); 
+    }else{
+        if( searchItems.find((item) => item === value)){
+            presentDrinks(`${LOCAL_URL}${value}.json`); 
+        }
+    }
 });
